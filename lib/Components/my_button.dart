9f23750586d0b585df45a37c95916b3ef066d27a
Fanelesiyibonge Mabuza <menzifm12@gmail.com>
2024-02-14 22:01:20 +0200

    final before push

--- lib/Components/my_button.dart
+++ lib/Components/my_button.dart
@@ -8,7 +8,7 @@ class MyButton extends StatelessWidget {
   const MyButton({super.key, required this.name, required this.onTap, required this.child});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return GestureDetector(
       onTap: onTap,
       child: Container(
